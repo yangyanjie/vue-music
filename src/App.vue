3b4d1e4d996d92e30f2919e8fb62122fdc125b88
yangyang <yangyanjie@ochain.fun>
2018-08-20 15:55:40 +0800

    add-11

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <template>
-  <div id="app" @touchmove.prevent>
-    <m-header></m-header>
-    <tab></tab>
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
-    <player></player>
+  <div id="app">
+    hello world
   </div>
 </template>
 
-<script type="text/ecmascript-6">
-  import MHeader from 'components/m-header/m-header'
-  import Player from 'components/player/player'
-  import Tab from 'components/tab/tab'
-
-  export default {
-    components: {
-      MHeader,
-      Tab,
-      Player
-    }
-  }
+<script>
+  
 </script>
+<style>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
 </style>
+
+
