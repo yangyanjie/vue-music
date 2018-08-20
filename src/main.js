72b5bcc58f5554d4d165ba71159d708cb1f4b69e
yangyang <yangyanjie@ochain.fun>
2018-08-20 16:09:19 +0800

    add-222

--- src/main.js
+++ src/main.js
@@ -1,27 +1,16 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-import fastclick from 'fastclick'
-import VueLazyload from 'vue-lazyload'
-import store from './store'
-
-import 'common/stylus/index.styl'
+//import router from './router'
 
 /* eslint-disable no-unused-vars */
 // import vConsole from 'vconsole'
 
-fastclick.attach(document.body)
-
-Vue.use(VueLazyload, {
-  loading: require('common/image/default.png')
-})
+//fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  store,
   render: h => h(App)
 })
 
